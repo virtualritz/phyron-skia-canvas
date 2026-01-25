a006fabaaef13b697ff0572cc96b9cbcad6234b0
Moritz Moeller <virtualritz@protonmail.com>
2026-01-25 23:12:42 +0100

    Disable fontconfig tools build
    FreeType 2.14+ dynamically loads HarfBuzz via dlopen/dlsym,
    which breaks static linking of fontconfig CLI tools. Since we
    only need libfontconfig.a, skip building the tools entirely.

--- containers/install-fontlibs.sh
+++ containers/install-fontlibs.sh
@@ -24,6 +24,6 @@ FONTCONFIG=fontconfig-$FONTCONFIG_VERSION
 FONTCONFIG_URL=https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/$FONTCONFIG_VERSION/${FONTCONFIG}.tar.xz
 curl -sL $FONTCONFIG_URL | tar xJf - -C /opt
 cd /opt/${FONTCONFIG} && \
-    meson setup -Dprefix=/ -Dsysconfdir=/etc -Dlocalstatedir=/var -Ddefault_library=static -Dprefer_static=true -Dxml-backend=expat -Dtests=disabled build && \
+    meson setup -Dprefix=/ -Dsysconfdir=/etc -Dlocalstatedir=/var -Ddefault_library=static -Dprefer_static=true -Dxml-backend=expat -Dtests=disabled -Dtools=disabled build && \
     meson compile -C build && \
     meson install --destdir=/usr/local -C build
